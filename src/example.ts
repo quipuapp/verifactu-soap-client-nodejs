aff3bc28e0e2e15f53d6166f34f6c6e65afbe527
Florent Lemaitre <flemaitre@sellsy.com>
2024-12-06 11:29:17 +0100

    update example

--- src/example.ts
+++ src/example.ts
@@ -1,17 +1,41 @@
+// this is an example of how to use the generated client
+// to make a SOAP call to the Verifactu service
+// not warranty that this code will work
+// you will need to adapt it to your needs
+
 import {
   RegistroAlta,
   SistemaInformatico,
 } from './generated/sistemafacturacion'
 import { createClientAsync } from './generated/sistemafacturacion/client'
 import { RegFactuSistemaFacturacion } from './generated/sistemafacturacion/definitions/RegFactuSistemaFacturacion'
+import fs from 'fs'
 
 async function main() {
+  // need a correct certificate and private key
+  // on production it's only possible to use a valid certificate
+  // use env var to store the content of the certificate and private key
+  const CERT_PATH = './certificate.crt'
+  const KEY_PATH = './private.key'
+
+  // Client options for the SOAP call
+  const options = {
+    wsdl_options: {
+      cert: fs.readFileSync(CERT_PATH), // Client certificate
+      key: fs.readFileSync(KEY_PATH), // Private key
+      strictSSL: false, // Allow self-signed certificates
+      rejectUnauthorized: false, // Disable SSL verification (use with caution)
+      disableCache: true, // Optional SOAP client options
+    },
+  }
+
   const client = await createClientAsync(
     'https://prewww2.aeat.es/static_files/common/internet/dep/aplicaciones/es/aeat/tikeV1.0/cont/ws/SistemaFacturacion.wsdl',
-    {
-      // Optional SOAP client options
-      disableCache: true,
-    },
+    options,
+  )
+
+  client.setEndpoint(
+    'https://prewww2.aeat.es/wlpl/TIKE-CONT/ws/SistemaFacturacion/VerifactuSOAP',
   )
 
   try {
@@ -78,9 +102,21 @@ async function main() {
 
     const result = await client.RegFactuSistemaFacturacionAsync(parameters)
 
+    // keep the requesti
+
     console.log('Result:', result)
   } catch (error) {
     console.error('Error:', error)
+  } finally {
+    // save the request into a xml file
+    const request = client.lastRequest
+
+    fs.writeFileSync('./request.xml', request?.toString() || '')
+
+    // save the response into a xml file
+    const response = client.lastResponse
+
+    fs.writeFileSync('./response.xml', response)
   }
 }
 
