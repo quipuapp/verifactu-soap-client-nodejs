269465e85348b28742c94a6b107f3c81c23ad03d
Florent Lemaitre <flemaitre@sellsy.com>
2024-12-09 08:15:50 +0000

    add constants and export all definitions

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * Version of message for RegistroAlta
+ */
+export const ID_VERSION_REGISTRO_ALTA = '1.0'
+
+export const TYPO_HUELLA = 'SHA-256'
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// export client and all types
+export * from './generated/sistemafacturacion/client'
+
+export * from './generated/sistemafacturacion/index'
